84aa26b4e0e26f3b4c523d82f4fe93942ab3a8d7
josie <jsphnmgt@gmail.com>
2025-10-11 11:04:43 +0800

    Add files via upload

---
+++ lib/models/stalls_model.dart
@@ -0,0 +1,38 @@
+class Stall {
+  final String id;
+  final String imagePath;
+  final String title;
+  final String status;
+  final bool isFavorited;
+  final String location;
+
+  Stall({
+    required this.id,
+    required this.imagePath,
+    required this.title,
+    required this.status,
+    this.isFavorited = false,
+    required this.location, 
+  });
+
+  factory Stall.fromMap(Map<String, dynamic> data, String id) {
+    return Stall(
+      id: id,
+      imagePath: data['imagePath'] ?? '',
+      title: data['title'] ?? '',
+      status: data['status'] ?? 'Closed',
+      isFavorited: data['isFavorited'] ?? false,
+      location: data['location'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'imagePath': imagePath,
+      'title': title,
+      'status': status,
+      'isFavorited': isFavorited,
+      'location': location, 
+    };
+  }
+}
