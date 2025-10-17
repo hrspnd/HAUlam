c088831b7e87bde79565b6c8540a23ecb973284e
hrspnd <8alexapineda8@gmail.com>
2025-10-17 17:33:30 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,18 @@
+/*
+  File: main.dart
+  Purpose: Initializes the HAUlam Flutter application, sets up Supabase
+           for backend services, enforces portrait orientation, and
+           configures global app theming and styles.
+  Developers: Magat, Maria Josephine M. [jsphnmgt]
+              Rebusa, Amber Kaia J. [juliankaiaaa]
+              Pineda, Mary Alexa Ysabelle V. [hrspnd]
+*/
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:haulam/auth-backend/auth_gate.dart';
-import 'package:haulam/auth-backend/auth_page.dart';
-import 'package:haulam/auth-backend/testpage.dart';
-import 'package:haulam/screens/account-pages/account_details.dart';
-import 'package:haulam/screens/customer-stall-pages/bookmarks.dart';
-import 'package:haulam/screens/customer-stall-pages/stalls.dart';
-import 'package:haulam/screens/customer-stall-pages/stall_dish_view.dart';
-import 'package:haulam/screens/account-pages/account_profile.dart';
-import 'package:haulam/screens/account-pages/twofactorauth.dart';
-import 'package:haulam/screens/account-pages/changepassword.dart';
-import 'package:haulam/screens/account-pages/resetpassword.dart';
-import 'package:haulam/screens/account-pages/editaccount.dart';
-import 'package:haulam/screens/signup-login-pages/loginpage.dart';
-import 'package:haulam/screens/signup-login-pages/successpage.dart';
-import 'package:haulam/screens/vendor-view-pages/create_stall.dart';
+import 'package:haulam/auth-backend/deep_link_handler.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'screens/signup-login-pages/otppage.dart';
-
-import 'screens/customer-stall-pages/stall_dishes.dart';
-// import 'screens/signup-login-pages/signuppage.dart';
-import 'screens/mainfour.dart';
-import 'screens/store_roof.dart';
-import 'package:haulam/screens/signup-login-pages/splashpage.dart';
-import 'models/menu_item.dart';
-import 'models/stalls_model.dart';
-
-import 'screens/maintwo.dart';
-import 'screens/vendor-view-pages/vendor_stall.dart';
-import 'screens/vendor-view-pages/vendor_dish_view.dart';
-import 'screens/vendor-view-pages/vendor_edit_profile.dart';
-import 'screens/customer-stall-pages/search.dart';
-
-// import 'screens/signup-login pages/splashpage.dart';
+import 'package:haulam/screens/signup-login-pages/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -41,13 +21,16 @@ void main() async {
     anonKey:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hd3l5bnh0d3p3d2Z2b21pd2N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDYyNDksImV4cCI6MjA3NTEyMjI0OX0.qyBc7-vbxYhgyNoRyGJ8PMIP_k6bHsrrblZxINGqxpA",
   );
-  // =======================
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]).then((_) {
-    runApp(const MyApp());
+    runApp(
+    DeepLinkHandler(
+      child: MyApp(),
+    ),
+  );
   });
 }
 
@@ -73,9 +56,9 @@ class MyApp extends StatelessWidget {
         ),
         // =================================
         textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Colors.black, //makes cursor black globally
-          selectionColor: Color(0x30710E1D), //highlight color
-          selectionHandleColor: Color(0xff710E1D), //handle color
+          cursorColor: Colors.black,
+          selectionColor: Color(0x30710E1D), 
+          selectionHandleColor: Color(0xff710E1D), 
         ),
         scaffoldBackgroundColor: Colors.white,
 
@@ -86,66 +69,16 @@ class MyApp extends StatelessWidget {
             borderSide: const BorderSide(color: Color(0xff65000F), width: 2),
           ),
         ),
+
+        // ===== SNACKBAR =====
+        snackBarTheme: SnackBarThemeData(
+      backgroundColor: const Color(0xffE2E2E2),
+      contentTextStyle: const TextStyle(
+        color: Colors.black,
+        fontSize: 14,
       ),
+      ), ),
 
-      // home: const StallsPage(),
-      // home: SignUpPage(showLoginPage: () {  },),
-      // home: const LoginPage(),
-      // home: const OtpPage(phoneNumber: '87132789123',),
-      // home: const SetUpPage(),
-      // home: const SuccessPage(),
-      // home: const AccountPage(),
-      // home: const AccountDetailsPage(),
-      // home: const TwoFactorAuthenticationPage(),
-      // home: const ChangePasswordPage(),
-      // home: const ResetPasswordPage(), 
-      // home: const EditAccountPage(), 
-      // home: const MainFourScaffold(), // <------------------- YAM
-      // home: const StoreRoof(),
-      // home: const BookmarksPage(),
-      // home: const MainTwoScaffold(),
-      // home: const VendorStallPage(),
-      // home: const VendorEditProfilePage(),
-      // home: VendorDishViewPage(),
-      // home: SearchPage(), 
-      // home: CreateStallPage(),
-
-      // home: StallDishesPage( // <------------------- JOSIE
-      //   stall: Stall(
-      //     id: '1',
-      //     imagePath: 'assets/png/image-square.png',
-      //     title: 'KIRSTENJOY',
-      //     status: 'Open',
-      //     location: 'St. Martha Hall'
-      //   ),
-      // ),
-
-      // home: StallDishViewPage(
-      //   stall: Stall(
-      //     id: 'sample-id',
-      //     imagePath: 'assets/sample.png',
-      //     title: 'KIRSTENJOY',
-      //     status: 'Open',
-      //     isFavorited: false,
-      //     location: 'PGN Basement',
-      //   ),
-      //   dish: MenuItem(
-      //     id: 'dish-1',
-      //     name: 'Tocino',
-      //     imagePath: 'assets/png/tocino.png',
-      //     price: 80,
-      //     description: 'Chopped pork seasoned with spices, onions, and calamansi, served on a sizzling plate for a zesty, savory experience.',
-      //     tags: ['Pork', 'Spicy', 'Something', 'LOLS'],
-      //   ),
-      // ),
-
-      // home: TestProfilePage(),      <---- test info fetcher
-
-      // home: const Wrapper(),
-
-      // home: const AuthGate()
-
-      // eto yung totoong home
       home: const SplashPage(),
     );
   }
